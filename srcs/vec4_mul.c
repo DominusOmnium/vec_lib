4edc922a5e6983e55c6a512c5438eea3aa4e3c17
Dake Kathleen <dkathlee@am-a2.msk.21-school.ru>
2020-09-11 15:26:01 +0300

    Norminette

--- srcs/vec4_mul.c
+++ srcs/vec4_mul.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   vec4_mul.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: dkathlee <dkathlee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/18 08:09:55 by marvin            #+#    #+#             */
-/*   Updated: 2020/08/18 08:09:57 by marvin           ###   ########.fr       */
+/*   Updated: 2020/09/11 15:23:25 by dkathlee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vec_lib.h"
 
-t_ivec4		ivec4_mul_ivec4(t_ivec4 v1, t_ivec4 v2)
+t_ivec4	ivec4_mul_ivec4(t_ivec4 v1, t_ivec4 v2)
 {
 	return ((t_ivec4){v1.x * v2.x, v1.y * v2.y, v1.z * v2.z, v1.w * v2.w});
 }
